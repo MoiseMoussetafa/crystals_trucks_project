39579e0693c33eda226964dee0033986a33ab936
MoiseMoussetafa <moise.moussetafa@ynov.com>
2022-04-22 08:35:49 +0200

    remove useless test + add precommit yml

--- test_2.py
+++ test_2.py
@@ -5,8 +5,3 @@ def test_max_values():
     assert game.MAX_NB_TRUCKS == 9
     assert game.MAX_WIDTH == 30
     assert game.MAX_HEIGHT == 20
-
-
-def test_max_grid():
-    assert game.width <= game.MAX_WIDTH
-    assert game.height <= game.MAX_HEIGHT
